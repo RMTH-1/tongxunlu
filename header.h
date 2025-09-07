75aba772126f791e8f1398419ec3a18f57b4b374
Xingwang Zhao <1879023367@qq.com>
2025-09-07 10:40:38 +0800

    添加项目文件。

---
+++ header.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+#include <string>
+#include <vector>
+
+struct member {
+	string name;
+	string gender;
+	int age;
+	string phone;
+	string address;
+};
